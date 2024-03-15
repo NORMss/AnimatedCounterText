80969e2bcb4bd62d5d515e266361e5f49a0f430f
NORMss <serega.b301@gmail.com>
2024-03-15 22:44:52 +0700

    feat: added buttons for increment and decrement, added text animation
    Closes #1

---
+++ app/src/main/java/com/norm/myanimatedcountertext/AnimatedCounter.kt
@@ -0,0 +1,73 @@
+@file:OptIn(ExperimentalAnimationApi::class)
+
+package com.norm.myanimatedcountertext
+
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+
+@Composable
+fun AnimatedCounter(
+    count: Int,
+    modifier: Modifier = Modifier,
+    style: TextStyle = MaterialTheme.typography.bodyLarge
+) {
+    var oldCount by remember {
+        mutableStateOf(count)
+    }
+    SideEffect {
+        oldCount = count
+    }
+    Row(
+        modifier = modifier
+    ) {
+        val countString = count.toString()
+        val oldCountString = oldCount.toString()
+        for (i in countString.indices) {
+            val oldChar = oldCountString.getOrNull(i)
+            val newChar = countString[i]
+            val char = if (oldChar == newChar) {
+                oldCountString[i]
+            } else {
+                countString[i]
+            }
+            AnimatedContent(
+                targetState = char,
+                transitionSpec = {
+                    if (oldCount < count) {
+                        slideInVertically {
+                            it
+                        } togetherWith slideOutVertically {
+                            -it
+                        }
+                    } else {
+                        slideInVertically {
+                            -it
+                        } togetherWith slideOutVertically {
+                            it
+                        }
+                    }
+                }, label = ""
+            ) { char ->
+                Text(
+                    text = char.toString(),
+                    style = style,
+                    softWrap = false
+                )
+            }
+        }
+    }
+}
